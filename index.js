0a45b6aecdac6faeae7b935b402eff114daa5a87
Aubrey Portwood <code@aubreypwd.com>
2019-07-26 12:39:11 -0700

    Remove rules that are already defined in WP extends

--- index.js
+++ index.js
@@ -91,85 +91,6 @@ module.exports = {
 		'wds-js-coding-standards-eslint-custom/functionExpressionRequireDocblock': 2,
 
 		/**
-		 * Enforce spacing inside array brackets.
-		 *
-		 * @standard WDS
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'array-bracket-spacing': [ 'error', 'always' ],
-
-		/**
-		 * Enforce one true brace style.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#blocks-and-curly-braces
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'brace-style': 'error',
-
-		/**
-		 * Require camel case names.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#naming-conventions
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'camelcase': [ 'error', {
-			properties: 'always'
-		} ],
-
-		/**
-		 * Disallow or enforce trailing commas.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#objects
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'comma-dangle': [ 'error', 'never' ],
-
-		/**
-		 * Enforce spacing before and after comma.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#objects
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'comma-spacing': 'error',
-
-		/**
-		 * Enforce one true comma style.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'comma-style': [ 'error', 'last' ],
-
-		/**
 		 * Encourages use of dot notation whenever possible.
 		 *
 		 * @standard WP
@@ -177,6 +98,7 @@ module.exports = {
 		 *
 		 * @author   Aubrey Portwood
 		 * @since    1.0
+		 * @since    7/26/2019 Overrides es5 from WP.
 		 *
 		 * @report   Error
 		 */
@@ -186,18 +108,6 @@ module.exports = {
 		} ],
 
 		/**
-		 * Enforce newline at the end of file, with no multiple empty lines.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'eol-last': 'error',
-
-		/**
 		 * Require or disallow spacing between function identifiers and their invocations.
 		 *
 		 * @standard WP
@@ -205,6 +115,7 @@ module.exports = {
 		 *
 		 * @author   Aubrey Portwood
 		 * @since    1.0
+		 * @since    7/26/2019 Overrides WP.
 		 *
 		 * @report   None
 		 */
@@ -219,37 +130,14 @@ module.exports = {
 		 * @author   Aubrey Portwood
 		 * @since    1.0
 		 *
+		 * @since    7/26/2019 Overrides WP.
+		 *
 		 * @report   Error
 		 */
 		'key-spacing': [ 'error', {
 			beforeColon: false,
 			afterColon: true
 		} ],
-
-		/**
-		 * Enforce spacing before and after keywords.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'keyword-spacing': 'error',
-
-		/**
-		 * Disallow mixed "LF" and "CRLF" as linebreaks.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'linebreak-style': [ 'error', 'unix' ],
 
 		/**
 		 * Enforces empty lines around comments.
@@ -266,158 +154,6 @@ module.exports = {
 		} ],
 
 		/**
-		 * Disallow mixed spaces and tabs for indentation.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'no-mixed-spaces-and-tabs': 'error',
-
-		/**
-		 * Disallow use of multiline strings.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'no-multi-str': 'error',
-
-		/**
-		 * Disallow multiple empty lines.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'no-multiple-empty-lines': 'error',
-
-		/**
-		 * Disallow use of the with statement.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'no-with': 'error',
-
-		/**
-		 * Require or disallow an newline around variable declarations.
-		 *
-		 * @standard WDS
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'one-var-declaration-per-line': [ 'error', 'initializations' ],
-
-		/**
-		 * Enforce operators to be placed before or after line breaks.
-		 *
-		 * @standard WP
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'operator-linebreak': [ 'error', 'after' ],
-
-		/**
-		 * Require or disallow use of semicolons instead of ASI.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#semicolons
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'semi': [ 'error', 'always' ],
-
-		/**
-		 * Require or disallow space before blocks.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'space-before-blocks': [ 'error', 'always' ],
-
-		/**
-		 * Require or disallow space before function opening parenthesis.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.0
-		 *
-		 * @report   Error
-		 */
-		'space-before-function-paren': [ 'error', 'never' ],
-
-		/**
-		 * Require or disallow space before blocks.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'space-in-parens': [ 'error', 'always' ],
-
-		/**
-		 * Require spaces around operators.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.1
-		 *
-		 * @report   Error
-		 */
-		'space-infix-ops': 'error',
-
-		/**
-		 * Require or disallow spaces before/after unary operators (words on by default, nonwords),
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'space-unary-ops': [ 'error', {
-			overrides: { '!': true }
-		} ],
-
-		/**
 		 * Don't force vars to be on top.
 		 *
 		 * In contradiction to WP Coding Standards,
@@ -429,6 +165,8 @@ module.exports = {
 		 * @since    1.1
 		 * @author   Aubrey Portwood
 		 *
+		 * @since    7/26/2019 Overrides WP.
+		 *
 		 * @report   None
 		 */
 		'vars-on-top': 'off',
@@ -436,7 +174,7 @@ module.exports = {
 		/**
 		 * Require or disallow Yoda conditions.
 		 *
-		 * @standard WP
+		 * @standard WDS
 		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#yoda-conditions
 		 *
 		 * @since  1.0
@@ -445,64 +183,6 @@ module.exports = {
 		 * @report   Error
 		 */
 		'yoda': [ 'error', 'always' ],
-
-		/**
-		 * Always show an error when a variable is created that is never used.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'no-unused-vars': 'error',
-
-		/**
-		 * No use of console.
-		 *
-		 * Use of console can be done safely with checking
-		 * window.console and running it from there.
-		 *
-		 * @standard WDS
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'no-console': 'error',
-
-		/**
-		 * No use of debugger.
-		 *
-		 * This is because we often can leave it in the code,
-		 * this draws a nice red line around it.
-		 *
-		 * @standard WDS
-		 *
-		 * @since  1.1
-		 * @author Aubrey Portwood
-		 *
-		 * @report   Error
-		 */
-		'no-debugger': 'error',
-
-		/**
-		 * Require valid jsdoc blocks.
-		 *
-		 * @standard WDS
-		 *
-		 * @since  1.1
-		 * @author Aubrey Portwood
-		 *
-		 * @report   Error
-		 */
-		'valid-jsdoc': [ 'error', {
-
-			// If and only if the function or method has a return statement (this option value does apply to constructors)
-			'requireReturn': false
-		} ],
 
 		/**
 		 * Require docblocks.
@@ -544,46 +224,9 @@ module.exports = {
 		'func-style': [ 'error', 'declaration' ],
 
 		/**
-		 * Require == and !== where necessary.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#equality
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'eqeqeq': 'error',
-
-		/**
-		 * Require that braces be used.
-		 *
-		 * @standard WP
-		 *
-		 * E.g.
-		 *
-		 *     if ( foo ) return;
-		 *
-		 * would be bad, but
-		 *
-		 *     if ( foo ) {
-		 *         return;
-		 *     }
-		 *
-		 * would pass.
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'curly': 'error',
-
-		/**
 		 * Disallow null comparisons without type-checking operators.
 		 *
-		 * @standard WP
+		 * @standard WDS
 		 *
 		 * @since  1.1
 		 * @author Aubrey Portwood
@@ -607,50 +250,6 @@ module.exports = {
 		 *
 		 * @report   Error
 		 */
-		'radix': 'error',
-
-		/**
-		 * Force undefined variables to be in globals.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#globals
-		 *
-		 * E.g.
-		 *
-		 *     function a() {
-		 *
-		 *         // Below jQuery is undefined as it's included as a library.
-		 *         return jQuery( '#id' );
-		 *     }
-		 *
-		 * To fix:
-		 *
-		 *     // globals jQuery;
-		 *
-		 *     function a() {
-		 *
-		 *         // Below jQuery is undefined as it's included as a library.
-		 *         return jQuery( '#id' );
-		 *     }
-		 *
-		 * @author   Aubrey Portwood
-		 * @since    1.1
-		 *
-		 * @report   Error
-		 */
-		'no-undef': 'error',
-
-		/**
-		 * camelCaseAllTheThings.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#naming-conventions
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 *
-		 * @report   Error
-		 */
-		'camelcase': 'error'
+		'radix': 'error'
 	}
 };
