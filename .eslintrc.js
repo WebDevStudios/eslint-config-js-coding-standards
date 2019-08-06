0e371a922b05ed6ef7ce7b546cdb572cac3cabe6
George Gecewicz <gecewicz.george@gmail.com>
2019-08-06 11:43:14 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-	"extends": "@webdevstudios/eslint-config-js-coding-standards"
+	"extends": "@webdevstudios/eslint-config-js-coding-standards",
+	"env": {
+		"browser": true,
+		"jquery": true,
+		"es6": true
+	},
+	"parserOptions": {
+	   	"ecmaVersion": 6,
+	  	"sourceType": "module",
+	  	"allowImportExportEverywhere": true
+	}
 }
